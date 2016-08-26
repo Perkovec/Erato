9eb8b5a59809f450e90e21b029b0a67e8037338c
Perkovec <perkovec24@gmail.com>
2016-08-26 19:12:55 +0300

    Create new post path

--- bin/create.js
+++ bin/create.js
@@ -1,6 +1,25 @@
 const path = require('path');
-const utils = require('utils');
+const colors = require('ansicolors');
+const mkdirp = require('mkdirp');
+const utils = require('./utils');
+
+const redCross = colors.red('✗');
+const greenCheck = colors.green('✓');
+const cyan = colors.cyan;
+const red = colors.red;
 
 module.exports = (dir, title, layout) => {
-  
+  if (layout === 'post') {
+    const postsPath = path.join(dir, 'sources', 'posts');
+    if (!utils.existsDir(postsPath)) return console.log(red('could not find dir sources/posts/'));
+    const currentDate = new Date();
+    const dateVars = {
+      year: '' + currentDate.getFullYear(),
+      month: ('0' + (currentDate.getMonth() + 1)).slice(-2),
+      day: ('0' + currentDate.getDate()).slice(-2),
+    };
+    const todayPath = path.join(postsPath, dateVars.year, dateVars.month, dateVars.day);
+    console.log(todayPath);
+    if (!utils.existsDir(todayPath)) mkdirp.sync(todayPath);
+  }
 };
